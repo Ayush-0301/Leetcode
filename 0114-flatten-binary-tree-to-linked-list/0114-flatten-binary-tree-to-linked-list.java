08a69b00e6ec3b111c41b421b414f002da6643c9
Ayush-0301 <ayushmalik030@gmail.com>
2026-08-09 19:47:36 +0530

    Time: 0 ms (100%), Space: 43.8 MB (72.38%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -13,21 +13,20 @@
  *     }
  * }
  */
-
- ///Recursion se solve karra hai 
 class Solution {
     public void flatten(TreeNode root) {
-        if(root == null) return ;
-        TreeNode l = root.left;
-        TreeNode r = root.right;
-        root.left = null;
-        root.right = null;
-        flatten(l);
-        flatten(r);
-        root.right = l;
-        TreeNode last = root; // left ka lasst element hogaaa
-        while(last.right != null) last = last.right;
-        last.right =r ;
+        TreeNode curr = root;
+        while(curr!= null){
+            if(curr.left != null){
+                TreeNode pred = curr.left;
+                while(pred.right != null) pred = pred.right;
 
+                pred.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+
+            }
+            curr = curr.right;
+        }
     }
 }
