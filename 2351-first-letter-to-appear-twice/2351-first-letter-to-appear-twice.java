491964cd1e8b0a4fb4fe758e7e85b1f1acf39524
Ayush-0301 <ayushmalik030@gmail.com>
2026-08-29 22:39:38 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.93%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,12 +1,11 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        boolean [] freq = new boolean[26];
-        for(int i = 0;i<s.length();i++){
-        
-        if(freq[s.charAt(i)-'a']){
-         return s.charAt(i);}
-        
-        freq[s.charAt(i)-'a'] = true;
+        HashSet<Character> set = new HashSet<>();
+        for(char ch : s.toCharArray()){
+            if(set.contains(ch)){
+                return ch;
+            }
+            set.add(ch);
         }
         return ' ';
     }
