8585012cdf01ff32eff011dc101ccf55026c2dde
Ayush-0301 <ayushmalik030@gmail.com>
2026-09-11 20:08:04 +0530

    Time: 1 ms (98.42%), Space: 42.3 MB (91.19%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int temp = n;
+        int sum = 0;
+        while(temp > 0){
+            int digit  = temp % 10;
+            sum += digit;
+            temp /= 10;
+        }
+        return sum;
+    }
+}
